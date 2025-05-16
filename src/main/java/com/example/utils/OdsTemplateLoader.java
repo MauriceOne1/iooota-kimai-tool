2de3053deabf30e9cacdb82189d1b10e34928edf
Alan <alan@yggdrasil.net>
2025-05-16 18:34:56 +0200

    ADD: classe OdsTemplateLoader
    Questa classe si preoccupa di cercare e caricare il file di template da
    una serie di posizioni predefinite, in ordine di preferenza.
    
    Viene introdotta la dipendenza verso la libreria dev.dirs.directories
    per sfruttare la classe ProjectDirectories per determinare in maniera
    multipiattaforma la directory di configurazione dell'applicazione sotto
    la home dell'utente.

---
+++ src/main/java/com/example/utils/OdsTemplateLoader.java
@@ -0,0 +1,85 @@
+package com.example.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import com.example.KimaiOds;
+import com.example.Main;
+
+import dev.dirs.ProjectDirectories;
+
+/**
+ * Classe per gestire il caricamento del template per l'output in formato ODS.
+ * 
+ * <p>
+ * Il template da fornire è un file ODS che viene poi riempito
+ * programmaticamente dalla classe {@link KimaiOds}.
+ * 
+ * <p>
+ * Il file di template <code>{@value #TEMPLATE_FILE_NAME}</code> viene cercato
+ * nelle seguenti posizioni, in ordine di priorità:
+ * <ol>
+ * <li>sotto la directory {@value com.example.Main#APPLICATION_NAME} in un
+ * percorso sotto la propria $HOME (OS dependent)
+ * <li>directory attuale
+ * <li>classpath
+ * </ol>
+ * 
+ * @see ProjectDirectories
+ */
+public class OdsTemplateLoader {
+
+	/**
+	 * Nome atteso per il file di template (FISSO)
+	 */
+	public static final String TEMPLATE_FILE_NAME = "presenzeTemplate.ods";
+
+	/**
+	 * Directory sotto la home dell'utente attuale nella quale verrà cercato il file
+	 * di template, se presente.
+	 * 
+	 * @see ProjectDirectories#configDir
+	 */
+	private final Path homeConfigDir;
+
+	/**
+	 * Directory corrente, ovvero directory di lavoro attuale del processo JVM che
+	 * richiama questa classe.
+	 */
+	private final Path currentDir;
+
+	/**
+	 * Crea una nuova istanza di {@link OdsTemplateLoader} inizializzando i path di
+	 * ricerca interni.
+	 */
+	public OdsTemplateLoader() {
+		ProjectDirectories baseDirs = ProjectDirectories.from("com.example", "IOOOTA", Main.APPLICATION_NAME);
+		this.homeConfigDir = Paths.get(baseDirs.configDir);
+		this.currentDir = Paths.get("").toAbsolutePath();
+	}
+
+	/**
+	 * Ottiene un puntamento al file di template da utilizzare in base alle
+	 * politiche di ricerca attuali.
+	 * 
+	 * @throws IOException in caso di errore di I/O in apertura dell'InputStream
+	 */
+	public InputStream resolveTemplateFile() throws IOException {
+		Path templateInHomeCfg = homeConfigDir.resolve(TEMPLATE_FILE_NAME);
+		Path templateInCwd = currentDir.resolve(TEMPLATE_FILE_NAME);
+
+		if (Files.isRegularFile(templateInHomeCfg) && Files.isReadable(templateInHomeCfg)) {
+			return Files.newInputStream(templateInHomeCfg);
+		}
+
+		if (Files.isRegularFile(templateInCwd) && Files.isReadable(templateInCwd)) {
+			return Files.newInputStream(templateInCwd);
+		}
+
+		return ClassLoader.getSystemResourceAsStream(TEMPLATE_FILE_NAME);
+	}
+
+}
